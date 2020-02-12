4bcb6d16f3476d79612a67647262d6178178881b
Aluno <Aluno@trofa.pt>
2020-02-12 12:07:57 +0000

    sec commit

---
+++ client/src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import { Navbar, Nav } from 'react-bootstrap';
+import Header from './components/header/header';
+import Content from './components/contentMain/content';
+import Login from './components/forms/login';
+import Banner from './components/secBanner/secBanner';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar className="bg" bg="light" variant="light" sticky="top">
+          <Navbar.Brand href="#home">WhatToEat</Navbar.Brand>
+          <Navbar.Collapse className="justify-content-end">
+            <Nav>
+              <Nav.Link href="#login"><Login></Login></Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <Header></Header>
+        <div>
+          <Content></Content>
+        </div>
+        <div>
+          <Banner></Banner>
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ client/src/components/forms/login.js
@@ -0,0 +1,94 @@
+import React, { useState } from 'react';
+import './login.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Modal, Form, Col } from 'react-bootstrap';
+import Image from 'react-bootstrap/Image';
+import user from './user.png';
+
+
+
+function Login() {
+  const [show, setShow] = useState(false);
+  const [regShow, setRegShow] = useState(false);
+  return (
+    <>
+      <span onClick={() => setShow(true)}>
+        Login
+     </span>
+
+      <Modal show={show} id="modal1" onHide={() => setShow(false)} centered>
+        <Modal.Header closeButton> 
+          <div>
+            <Image src={user} alt="User" className="Image-user" roundedCircle />
+          </div>
+        </Modal.Header>
+
+        <Modal.Body>
+          <Form>
+            <Form.Row>
+              <Form.Group as={Col} controlId="formGridEmail">
+                <Form.Label>Email</Form.Label>
+                <Form.Control type="email" placeholder="Insira o seu email" />
+              </Form.Group>
+            </Form.Row>
+            <Form.Row>
+              <Form.Group as={Col} controlId="formGridPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+              </Form.Group>
+            </Form.Row>
+          </Form>
+
+        </Modal.Body>
+
+        <Modal.Footer>
+          <Button variant="success" type="submit">
+            Entrar
+  </Button>
+          <Button variant="danger"  onClick={() => setRegShow(true) || setShow(false)}>
+
+            registo
+            </Button>
+
+        </Modal.Footer>
+      </Modal>
+
+      {/* Modal 2*/}
+
+      <Modal show={regShow} onHide={() => setRegShow(false)} id="modal2" centered>
+        <Modal.Header closeButton>
+          <div>
+            <h2>Registo</h2>
+          </div>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Row>
+              <Form.Group as={Col} controlId="formGridEmail">
+                <Form.Label>Email</Form.Label>
+                <Form.Control type="email" placeholder="Insira o seu email" />
+              </Form.Group>
+            </Form.Row>
+            <Form.Row>
+              <Form.Group as={Col} controlId="formGridPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+              </Form.Group>
+            </Form.Row>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="success" type="submit" >
+            Entrar
+          </Button>
+          <Button variant="danger" onClick={() => setRegShow(false) || setShow(true)}>
+            login
+            </Button>
+
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
+
+export default Login;
