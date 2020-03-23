d17f2dcb46c4f5526e6149dfecb1b4795260a97b
SlimHeavyNutz <50211028+SlimHeavyNutz@users.noreply.github.com>
2020-03-23 16:04:06 +0000

    adjustment for header css

--- frontend/src/components/header/header.js
+++ frontend/src/components/header/header.js
@@ -1,19 +1,15 @@
-import React from 'react';
-import './header.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {Jumbotron} from 'react-bootstrap';
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import { Jumbotron } from "react-bootstrap";
+import "./header.css";
 
 function Header() {
   return (
     <div className="App">
-            <Jumbotron className="bg-img">
-                <h1>
-                 Saiba sempre o que cozinhar
-                </h1>
-                <p className="parag">
-                  e como cozinhar
-                </p>
-            </Jumbotron>
+      <Jumbotron className="bg-img">
+        <h1 className="Principal">Saiba sempre o que cozinhar</h1>
+        <p className="parag">e como cozinhar</p>
+      </Jumbotron>
     </div>
   );
 }
