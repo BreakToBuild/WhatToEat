6a60584bed8ec3906f171a8ab53c1556d6c72593
SlimHeavyNutz <eusebiodasilva417@gmail.com>
2020-06-18 10:08:40 +0100

    Added private Routes

---
+++ frontend/src/components/PrivateRoute.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Redirect, Route } from "react-router-dom";
+
+export const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={(props) =>
+      localStorage.getItem("token") ? (
+        <Component {...props} />
+      ) : (
+        <Redirect
+          to={{
+            pathname: "/",
+            state: { from: props.location },
+          }}
+        />
+      )
+    }
+  />
+);
+
+export default PrivateRoute;
