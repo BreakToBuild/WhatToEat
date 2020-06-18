6cf2e4fb1c934ded43df714597eddc0207107a56
SlimHeavyNutz <eusebiodasilva417@gmail.com>
2020-06-18 10:09:39 +0100

    Teste de Login E signUp c/Video

--- frontend/cypress/integration/examples/firstTest_spec.js
+++ frontend/cypress/integration/examples/firstTest_spec.js
@@ -1,37 +1,38 @@
 /// <reference types="Cypress" />
+describe("SignUp & SignIn test", () => {
+  it("verify request and response SignUp ", () => {
+    cy.visit("/");
+    cy.server();
+    cy.route("POST", "**/signup").as("signup");
 
-it("verify request and response SignUp ", () => {
-  cy.visit("http://dev.localhost:3000/signup");
-  cy.server();
-  cy.route("POST", "**/signup").as("signup");
+    cy.get("[data-cy=registar]").click();
+    cy.get('[name="first_name"]').type("Eusébio");
+    cy.get('[name="last_name"]').type("Silva");
+    cy.get('[name="email"]').type("tuzebioSilva@gmail.com");
+    cy.get('[name="password"]').type("adminteste");
+    cy.get("form").submit();
 
-  cy.get("[data-cy=registar]").click();
-  cy.get('[name="first_name"]').type("");
-  cy.get('[name="last_name"]').type("Pedro");
-  cy.get('[name="email"]').type("testedostestes@gmail.com");
-  cy.get('[name="password"]').type("simsim");
-  cy.get("form").submit();
-
-  cy.wait("@signup");
-  cy.get("@signup").then((xhr) => {
-    console.log(xhr);
-    expect(xhr.status).to.equal(201);
+    cy.wait("@signup");
+    cy.get("@signup").then((xhr) => {
+      console.log(xhr);
+      expect(xhr.status).to.equal(201);
+    });
   });
-});
 
-it("verify request and response Login ", () => {
-  cy.visit("http://dev.localhost:3000/login");
-  cy.server();
-  cy.route("POST", "**/login").as("login");
+  it.only("verify request and response Login ", () => {
+    cy.visit("/");
+    cy.server();
+    cy.route("POST", "**/login").as("login");
 
-  cy.get("[data-cy=Login]").click();
-  cy.get('[name="email"]').type("testedostestes@gmail.com");
-  cy.get('[name="password"]').type("simsim");
-  cy.get("form").submit();
+    cy.get("[data-cy=Login]").click();
+    cy.get('[name="email"]').type("tuzebioSilva@gmail.com");
+    cy.get('[name="password"]').type("adminteste");
+    cy.get("form").submit();
 
-  cy.wait("@login");
-  cy.get("@login").then((xhr) => {
-    console.log(xhr);
-    expect(xhr.status).to.equal(200);
+    cy.wait("@login");
+    cy.get("@login").then((xhr) => {
+      console.log(xhr);
+      expect(xhr.status).to.equal(200);
+    });
   });
 });
