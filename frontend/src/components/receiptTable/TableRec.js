97455007d688b9af5f57a53ed20c48fbb4adaf3d
SlimHeavyNutz <eusebiodasilva417@gmail.com>
2020-06-19 10:55:36 +0100

    use functions instead of class's

--- frontend/src/components/receiptTable/TableRec.js
+++ frontend/src/components/receiptTable/TableRec.js
@@ -1,32 +1,30 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import React, { Component } from "react";
+import React from "react";
 import { Table } from "react-bootstrap";
 
-class TableRec extends Component {
-  render() {
-    return (
-      <div>
-        <Table>
-          <thead>
-            <tr>
-              <th>Nome </th>
-              <th>Descrição</th>
-              <th>Ingredientes</th>
-              <th>Preparação</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-          </tbody>
-        </Table>
-      </div>
-    );
-  }
+function TableRec() {
+  return (
+    <div>
+      <Table>
+        <thead>
+          <tr>
+            <th>Nome </th>
+            <th>Descrição</th>
+            <th>Ingredientes</th>
+            <th>Preparação</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+        </tbody>
+      </Table>
+    </div>
+  );
 }
 
 export default TableRec;
