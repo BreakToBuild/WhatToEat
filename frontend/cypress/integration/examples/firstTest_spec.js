00195d28fdd01d34d47acfecd87144dee504a77b
SlimHeavyNutz <eusebiodasilva417@gmail.com>
2020-06-25 18:28:01 +0100

    test SignIn & SignUp error

--- frontend/cypress/integration/examples/firstTest_spec.js
+++ frontend/cypress/integration/examples/firstTest_spec.js
@@ -6,6 +6,25 @@ describe("SignUp & SignIn test", () => {
     cy.route("POST", "**/signup").as("signup");
 
     cy.get("[data-cy=registar]").click();
+    cy.get('[name="first_name"]').type("cypressteste123");
+    cy.get('[name="last_name"]').type("Silva");
+    cy.get('[name="email"]').type("cypressteste123@gmail.com");
+    cy.get('[name="password"]').type("adminteste");
+    cy.get("form").submit();
+
+    cy.wait("@signup");
+    cy.get("@signup").then((xhr) => {
+      console.log(xhr);
+      expect(xhr.status).to.equal(200);
+    });
+  });
+
+  it("verify request and response error at SignUp  ", () => {
+    cy.visit("/");
+    cy.server();
+    cy.route("POST", "**/signup").as("signup");
+
+    cy.get("[data-cy=registar]").click();
     cy.get('[name="first_name"]').type("Eusébio");
     cy.get('[name="last_name"]').type("Silva");
     cy.get('[name="email"]').type("tuzebioSilva@gmail.com");
@@ -13,9 +32,13 @@ describe("SignUp & SignIn test", () => {
     cy.get("form").submit();
 
     cy.wait("@signup");
+
+    cy.get(".error-message")
+      .contains("Ocorreu um erro, tente novamente")
+      .should("exist");
     cy.get("@signup").then((xhr) => {
+      expect(xhr.status).to.equal(400);
       console.log(xhr);
-      expect(xhr.status).to.equal(201);
     });
   });
 
@@ -25,7 +48,7 @@ describe("SignUp & SignIn test", () => {
     cy.route("POST", "**/login").as("login");
 
     cy.get("[data-cy=Login]").click();
-    cy.get('[name="email"]').type("tuzebioSilva@gmail.com");
+    cy.get('[name="email"]').type("cypressteste123@gmail.com");
     cy.get('[name="password"]').type("adminteste");
     cy.get("form").submit();
 
@@ -33,6 +56,27 @@ describe("SignUp & SignIn test", () => {
     cy.get("@login").then((xhr) => {
       console.log(xhr);
       expect(xhr.status).to.equal(200);
+    });
+  });
+
+  it("verify request and response error at login ", () => {
+    cy.visit("/");
+    cy.server();
+    cy.route("POST", "**/login").as("login");
+
+    cy.get("[data-cy=Login]").click();
+    cy.get('[name="email"]').type("DeusebioSilva@gmail.com");
+    cy.get('[name="password"]').type("adminteste");
+    cy.get("form").submit();
+    cy.wait("@login");
+
+    cy.get(".error-message")
+      .contains("Ocorreu um erro, tente novamente")
+      .should("exist");
+    cy.get("@login").then((xhr) => {
+      expect(xhr.status).to.equal(401);
+
+      console.log(xhr);
     });
   });
 });
