2067a5ff4f90b0fd216793ad075aa9c70e27111d
SlimHeavyNutz <eusebiodasilva417@gmail.com>
2020-07-14 10:30:51 +0100

    loaded_Data is creating 3 recipes

--- backend/core/management/commands/load_initial_data.py
+++ backend/core/management/commands/load_initial_data.py
@@ -1,4 +1,3 @@
-from django.contrib.auth.models import Group
 from django.core.management import call_command
 from django.core.management.base import BaseCommand
 
@@ -28,22 +27,56 @@ class Command(BaseCommand):
                 call_command("reset_db")
             call_command("migrate", interactive=False)
 
-        create_user("user@email.com", "Manuel", "Silva", "123456")
+        user = create_user("user@email.com", "Manuel", "Silva", "123456")
 
         # ---------- EXAMPLES ---------- #
 
-        category = Category.objects.create(name="Portuguesa")
-        category = Category.objects.create(name="Chinesa")
+        portuguesa = Category.objects.create(name="Portuguesa")
+        chinesa = Category.objects.create(name="Chinesa")
+        indiana = Category.objects.create(name="Indiana")
 
-        # # recepi creation
-        # recepi = Recepi.objects.create(
-        #     name="",
-        #     description="",
-        #     ingredients=[
-        #         {"name": "", "portion": "", "unit": "", "comment": ""}
-        #     ],  # object list
-        #     preparation="",
-        # )
+        sardinhas_na_cataplana = Recepi.objects.create(
+            name="Sardinhas na cataplana",
+            description="Tudo num só tacho, deixe que os alimentos libertem os sabores, que se vão misturando.",
+            ingredients=[
+                {"name": "Sardinhas", "portion": "7", "unit": "", "comment": ""},
+                {"name": "sal", "portion": "3", "unit": "", "comment": ""},
+                {"name": "Alho", "portion": "4", "unit": "", "comment": ""},
+            ],
+            preparation="1. Lave cuidadosamente as sardinhas em água fria corrente e certifique-se de que elimina todas as escamas. Enxugue-as em papel de cozinha, tempere-as com uma colher de chá de sal e reserve.  2. Descasque as cebolas, o alho e as batatas e corte-os em rodelas. Lave o tomate e os pimentos e limpe-os de sementes. Corte o tomate e os pimentos em rodelas.",
+            created_by=user,
+        )
+        pizza_com_peras = Recepi.objects.create(
+            name="Pizza com peras e gorgonzola",
+            description="Prepare esta pizza em 15 minutos. Tenha pronta a base de massa fina grelhe as peras, adicione o queijo gorgonzola e a rúcula. Fácil e deliciosa.",
+            ingredients=[
+                {"name": "Base de pizza", "portion": "1", "unit": "", "comment": ""},
+                {"name": "Peras", "portion": "300", "unit": "g", "comment": ""},
+                {
+                    "name": "Queijo gorgonzola",
+                    "portion": "330",
+                    "unit": "g",
+                    "comment": "",
+                },
+            ],
+            preparation="1. Lave cuidadosamente as sardinhas em água fria corrente e certifique-se de que elimina todas as escamas. Enxugue-as em papel de cozinha, tempere-as com uma colher de chá de sal e reserve.  2. Descasque as cebolas, o alho e as batatas e corte-os em rodelas. Lave o tomate e os pimentos e limpe-os de sementes. Corte o tomate e os pimentos em rodelas.",
+            created_by=user,
+        )
+        pica_pau_portuguesa = Recepi.objects.create(
+            name="pica-pau à portuguesa",
+            description="A receita de pica-pau tem origem na charneca do ribatejo, consiste em carne salteada temperada com ingredientes como o alho, o louro e vinho branco.",
+            ingredients=[
+                {"name": "Carne", "portion": "400", "unit": "g", "comment": ""},
+                {"name": "Alho", "portion": "3", "unit": "", "comment": ""},
+                {"name": "Louro", "portion": "1", "unit": "folha", "comment": ""},
+            ],
+            preparation="1 Tempere a carne com sal, pimenta e um dente de alho picado 2. Em seguida aqueça uma frigideira antiaderente e salteie os pimentos durante 2 minutos até ficarem dourados. Retire e reserve.",
+            created_by=user,
+        )
 
-        # # adding a category to a recepi
-        # RecepiCategories.objects.create(recepi=recepi, categor=category)
+        # adding a category to a recepi
+        RecepiCategories.objects.create(
+            recepi=sardinhas_na_cataplana, category=portuguesa
+        )
+        RecepiCategories.objects.create(recepi=pizza_com_peras, category=portuguesa)
+        RecepiCategories.objects.create(recepi=pica_pau_portuguesa, category=portuguesa)
